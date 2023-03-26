77325efbeae77d214346626358bdb90c42ede1d5
Marsel <marsel5555@yandex.ru>
2023-03-26 18:31:09 +0300

    2

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк, 
+// длина которых меньше либо равна 3 символа. Первоначальный массив можно ввести с клавиатуры, 
+// либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, 
+// лучше обойтись исключительно массивами.
+// Пример:
+// ["hello", "2", "world", ":-)"] -> ["2", ":-)"]
+
+string[] array = { "hello", "2", "world", ":-)"};
+int count = ArrayCheck(array);
+string[] resultArray = FillResultArray(array, count);
+int ArrayCheck(string[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3) count++;
+    }
+    return count;
+}
+string[] FillResultArray(string[] array, int count)
+{
+    string[] result = new string[count];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            result[result.Length - count] = array[i];
+            count--;
+        }
+    }
+    return result;
+}
+void PrintArray(string[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length - 1; i++) Console.Write($"{array[i]}, ");
+    Console.Write($"{array[array.Length - 1]}]");
+}
+PrintArray(resultArray);
